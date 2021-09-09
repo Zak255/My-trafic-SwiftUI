bef540d4e690b6f727b5087b7c83ee3f38df4a1f
Zakir.Kazimov <ramzan666666@gmail.com>
2021-09-09 19:22:21 +0300

    Initial Commit

---
+++ My trafic SwiftUI/My_trafic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_trafic_SwiftUIApp.swift
+//  My trafic SwiftUI
+//
+//  Created by Zakir on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct My_trafic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
